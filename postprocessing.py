221f18ec5d68b3f50fef80b6fa6db3d96584e09e
Richard Palmer <r.palmer@vam.ac.uk>
2022-04-23 18:37:02 +0100

    Refactored

--- postprocessing.py
+++ postprocessing.py
@@ -3,71 +3,44 @@ import json
 from datetime import datetime
 from pathlib import Path
 
-museumCounts = {}
-archiveCounts = {}
+stats = {}
 
 # Read downloaded filename
 
 with open('collection-stats.json') as json_file:
     data = json.load(json_file)
-    museumCount = data['objects']
-    archiveCounts = data['archives']
+    stats['museum'] = data['objects']
+    stats['archive'] = data['archives']
 
 # Read in existing CSV
 
 now = datetime.now()
 today = now.strftime("%d-%b-%Y")
-file_date = now.strftime("%b-%Y")
+file_year = now.year
 
-# Handle Museum Stats
-current_filename = f"museum-stats-{file_date}.csv"
 
-stats_csv = Path(current_filename)
+for institution in ('museum', 'archive'):
+  for count_type in ('records', 'images'):
 
-month_data = []
+   # Handle Museum Stats
+   current_filename = f"{institution}-{count_type}-{file_year}.csv"
+   stats_csv = Path(current_filename)
+   year_data = []
 
-if stats_csv.is_file():
+   if stats_csv.is_file():
     # Read in existing file
     with open(stats_csv) as csv_file:
         csvreader = csv.reader(csv_file)
         for row in csvreader:
-            month_data.append(row)
+            year_data.append(row)
 
-# Append todays date
+   # Append todays date
 
-month_data.append([now.year,now.month,now.day, 'records', museumCount['records']])
-month_data.append([now.year,now.month,now.day, 'images', museumCount['images']])
+   year_data.append([now.year,now.month,now.day, count_type, stats[institution][count_type]])
 
-# Write the file out
+   # Write the file out
 
-with open(current_filename, "w") as csv_file:
-    csvwriter = csv.writer(csv_file)
-    for row in month_data:
-        csvwriter.writerow(row)
-
-
-# Handle Archive Stats
-current_filename = f"archive-stats-{file_date}.csv"
-
-stats_csv = Path(current_filename)
-
-month_data = []
-
-if stats_csv.is_file():
-    # Read in existing file
-    with open(stats_csv) as csv_file:
-        csvreader = csv.reader(csv_file)
-        for row in csvreader:
-            month_data.append(row)
-
-# Append todays date
-
-month_data.append([now.year,now.month,now.day, 'records', archiveCount['records']])
-month_data.append([now.year,now.month,now.day, 'images', archiveCount['images']])
-
-# Write the file out
-
-with open(current_filename, "w") as csv_file:
-    csvwriter = csv.writer(csv_file)
-    for row in month_data:
+   with open(current_filename, "w") as csv_file:
+     csvwriter = csv.writer(csv_file)
+     for row in month_data:
         csvwriter.writerow(row)
